8f66f1bb88c815e0f83de3838cf8ac3df2b932b4
enamu <guyus5594@gmail.com>
2023-01-26 21:24:56 +0900

    애노테이션 PostConstruct, PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -23,14 +26,13 @@ public class NetworkClient {
     public void disconnect(){
         System.out.println("close: " + url);
     }
-
-    public void init() throws Exception{
+    @PostConstruct
+    public void init() {
         System.out.println("NetworkClient.destroy");
         connect();
         call("초기화 연결 메시지");
     }
-
-
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();

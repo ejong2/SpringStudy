649cb3a69a48760140ee1b5f3c6b7bb35d43dd81
enamu <guyus5594@gmail.com>
2023-01-26 23:10:45 +0900

    request 스코프 예제 만들기

--- src/test/java/hello/core/scope/SingletonWIthPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWIthPrototypeTest1.java
@@ -3,11 +3,15 @@ package hello.core.scope;
 import ch.qos.logback.core.net.server.Client;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -34,16 +38,15 @@ public class SingletonWIthPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }

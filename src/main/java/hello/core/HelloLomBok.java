186158122e9c67fcb8623c515bca4efa73c49e66
enamu <guyus5594@gmail.com>
2023-01-26 19:29:20 +0900

    롬복 사용

---
+++ src/main/java/hello/core/HelloLomBok.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLomBok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLomBok helloLomBok = new HelloLomBok();
+        helloLomBok.setName("asdfg");
+        System.out.println("helloLomBok = " + helloLomBok);
+    }
+}
